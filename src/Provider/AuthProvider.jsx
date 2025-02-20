7b89284b4572f2465eeef9703fd45ba4b4bd413c
MdRezaulHoq <muradsust3@gmail.com>
2025-02-20 17:11:25 +0600

    AuthProvider Update: google, github, loginWithEmail, registerWithEmail etc auth Added

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,22 +1,60 @@
-import { createContext } from "react";
-import { app } from "../Firebase/firebase.init";
+import { createContext, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import auth from "../Firebase/firebase.init";
 
 export const AuthContext = createContext(null);
 
-const auth = getAuth(app);
-
 const AuthProvider = ({ children }) => {
-  const authInfo = {
-    user,
-    setUser,
-    loading,
-    createUser,
-    userLogin,
-    userLogout,
-    UserProfile,
-    logInbyGoogle,
-    logInbyGithub,
+  // google and github provider
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
+  // user and loading state
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Register with Email & Password
+  const userRegisterWithEmail = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
   };
+
+  // Google auth provider
+  const googleLogin = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // github provider
+  const logInbyGithub = () => {
+    return signInWithPopup(auth, githubProvider);
+  };
+
+  // login With Email
+  const userLoginWithEmail = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // logout auth
+  const userLogout = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  // Update Profile
+  const userProfileUpdate = (updateData) => {
+    return updateProfile(auth.currentUser, updateData);
+  };
+  // Auth Info
+  const authInfo = {};
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
